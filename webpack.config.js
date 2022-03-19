6b6d99e3c66f800a2d3da07246c3f57a3b3aff74
endezz <r-serega13@yandex.ru>
2022-03-19 21:42:38 +0400

    feat: add header & screen layout & update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = (options) => {
-  const isDev = options.name === 'development';
+module.exports = (env, options) => {
+  const isDev = options.mode === 'development';
 
   const config = {
     mode: isDev ? 'development' : 'production',
     devtool: isDev ? 'inline-source-map' : false,
     devServer: {
-      hot: true,
+      // hot: true,
       open: true,
       compress: true,
       port: 8080,
+      historyApiFallback: true,
+      static: __dirname + '/src/',
     },
-    entry: {
-      main: path.resolve(__dirname, './src/index.js'),
-    },
+    context: path.join(__dirname, './src'),
+    entry: path.join(__dirname, './src', 'index.js'),
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].[contenthash].js',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './src/index.html'),
+        template: path.join(__dirname, 'src', 'index.html'),
       }),
       new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
       new MiniCssExtractPlugin({
@@ -38,8 +39,8 @@ module.exports = (options) => {
       new ImageminPlugin({
         test: /\.(png|gif|svg|jpg|jpeg)$/i,
       }),
-      new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] }),
-      new StylelintPlugin({ extensions: ['css', 'scss', 'sass'] }),
+      // new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] }),
+      // new StylelintPlugin({ extensions: ['css', 'scss', 'sass'] }),
     ],
     module: {
       rules: [
@@ -56,8 +57,8 @@ module.exports = (options) => {
           test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
           type: 'asset/resource',
           generator: {
-            filename: 'assets/[hash][ext][query]'
-          }
+            filename: 'assets/[hash][ext][query]',
+          },
         },
         {
           test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
@@ -65,16 +66,39 @@ module.exports = (options) => {
         },
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+          ],
         },
         {
           test: /\.(s[ac]ss)$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
         },
       ],
     },
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new CssMinimizerPlugin()],
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            type: 'css/mini-extract',
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
     },
   };
 
