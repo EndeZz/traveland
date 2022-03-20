b9d3ba59a7f4b2092658ead3ee689cd55973ba2f
endezz <r-serega13@yandex.ru>
2022-03-20 21:18:34 +0400

    fix: update slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,44 +1,32 @@
 'use strict';
 
-const container = document.querySelector('.offers__container');
-const track = document.querySelector('.offers__group');
-const items = document.querySelectorAll('.offers__card');
-const btnPrev = document.querySelector('.btn-prev');
+const slider = document.querySelector('.offers__group');
 const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
+const item = document.querySelector('.offers__item');
 
-let position = 0;
-const slideToShow = 4;
-const slideToScroll = 1;
+let itemleft = 0;
+let itemline = 0;
 
-const itemsCount = items.length;
-const itemWidth = container.clientWidth / slideToShow;
-const movePosition = slideToScroll * itemWidth;
-
-btnNext.addEventListener('click', () => {
-  const itemsLeft = itemsCount - (Math.abs(position) + slideToShow * itemWidth) / itemWidth;
-
-  position -= itemsLeft >= slideToScroll ? movePosition : itemsLeft * itemWidth;
-
-  setPosition();
-  checkBtns();
+btnNext.addEventListener('click', function () {
+  itemleft = itemleft - 325;
+  itemline = itemline + 46;
+  if (itemleft < -1300) {
+    itemleft = -1300;
+    itemline = 180;
+  }
+  slider.style.left = `${itemleft}px`;
+  item.style.left = `${itemline}px`;
 });
 
-btnPrev.addEventListener('click', () => {
-  const itemsLeft = Math.abs(position) / itemWidth;
+btnPrev.addEventListener('click', function () {
+  itemleft = itemleft + 325;
+  itemline = itemline - 46;
 
-  position += itemsLeft >= slideToScroll ? movePosition : itemsLeft * itemWidth;
-  console.log(position);
-  setPosition();
-  checkBtns();
+  if (itemleft > 0) {
+    itemleft = 0;
+    itemline = 0;
+  }
+  slider.style.left = `${itemleft}px`;
+  item.style.left = `${itemline}px`;
 });
-
-const setPosition = () => {
-  track.style.transform = `translateX(${position * 0.06}%)`;
-};
-
-const checkBtns = () => {
-  btnPrev.disabled = position === 0;
-  btnNext.disabled = position <= -(itemsCount - slideToShow) * itemWidth;
-};
-
-checkBtns();
